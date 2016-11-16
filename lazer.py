7acb1876e592727e930c4ca71b4f7162815859a1
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-11-16 13:11:00 -0700

    add bounces for lazers

--- lazer.py
+++ lazer.py
@@ -4,7 +4,8 @@ class Lazer(object):
 	def __init__(self, px, py):
 		self.x = px
 		self.y = py
-		bounces = 0
+		self.bounces = 0
+		self.alive = True
 		self.image = pygame.image.load("lazer.png")
 		cx = pygame.mouse.get_pos()[0] - px
 		cy = pygame.mouse.get_pos()[1] - py
@@ -13,8 +14,20 @@ class Lazer(object):
 		self.vy = ((cy / div) * 2)
 
 	def render(self, screen):
-		screen.blit(self.image, (self.x, self.y))
+		if self.alive:
+			screen.blit(self.image, (self.x, self.y))
 
 	def update(self):
 		self.x += self.vx
 		self.y += self.vy
+		if self.x <= 0 or self.x >= 640:
+			self.vx = -self.vx
+			self.bounces += 1
+		if self.y <= 0 or self.y >= 480:
+			self.vy = -self.vy
+			self.bounces += 1
+		if self.bounces >= 4:
+			self.alive = False
+
+	def getAlive(self):
+		return self.alive
