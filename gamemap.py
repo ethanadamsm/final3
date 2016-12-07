bb8d98f0d52cb8b4d9adfd136f0d12d23608fd72
Ethan Michael Adams <adamsem@dcsdk12.org>
2016-12-07 13:13:48 -0700

    add spots for map

--- gamemap.py
+++ gamemap.py
@@ -1,13 +1,19 @@
 import sys, pygame, block
 
 class GameMap(object):
-	def __init__(self, filename):
+	def __init__(self, filename, spotfile):
 		self.filename = filename
+		self.spotfile = spotfile
 		self.blocks = []
+		self.spawnlocals = []
 		file = open(filename, "r")
+		file2 = open(spotfile, "r")
 		for line in file:
 			splitstring = [x.strip() for x in line.split(',')]
 			self.blocks.append(block.Block(int(splitstring[0]), int(splitstring[1]), int(splitstring[2]), int(splitstring[3]), splitstring[4]))
+		for line in file2:
+			splitstring = [x.strip() for x in line.split(',')]
+			self.spawnlocals.append([float(splitstring[0]), float(splitstring[1])])
 
 	def render(self, screen):
 		for block in self.blocks:
@@ -19,3 +25,6 @@ class GameMap(object):
 
 	def getBlocks(self):
 		return self.blocks
+
+	def getSpawns(self):
+		return self.spawnlocals
